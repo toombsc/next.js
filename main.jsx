4786e53c4b53053b6868207ca017d03e33bf5941
toombsc <carrietoombs@gmail.com>
2026-04-16 14:49:10 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,8 @@
+import React from "https://esm.sh/react";
+import ReactDOM from "https://esm.sh/react-dom/client";
+
+function App() {
+  return <h1>Deal Flow: New Orleans Commercial Real Estate</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
